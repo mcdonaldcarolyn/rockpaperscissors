e7ef4b26170a909dee5ec938224f0e8709f4e1a3
Carolyn Whelpley <33427006+mcdonaldcarolyn@users.noreply.github.com>
2024-01-18 20:31:23 +0000

    create quit loop

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ ties = 0
 
 while True:
     while True:
-        print 
+        print('{} Wins, {} losses, {} ties'.format(wins, losses, ties))
+        print('Enter your move: (R)ock (P)aper (S)cissors or (Q)uit')
+        playerMove = input('> ').upper()
+        if playerMove == 'Q':
+            print('Thanks for playing!')
+            sys.exit()
